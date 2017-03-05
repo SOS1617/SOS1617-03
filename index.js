2543da946e4d5e452c3e28b3b727be7fb8ac1eb0
SOS1617_rvs <rvizuetes@gmail.com>
2017-03-05 16:37:21 +0000

    Actualizacion del index.js 05/03/2017

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var moment = require("moment");
 var port = (process.env.PORT || 16778);
+var path = require("path");
 
 var app = express();
+var publicFolder = path.join(__dirname, 'public');
 
 var time = moment().utcOffset(+1).format('Do MMMM of YYYY, HH:mm:ss');
+
+app.use("/",express.static(publicFolder));
 
 app.get("/time",(request,response) => {
     response.send("<html><body><h1>"+time+"</h1></body></html>")
