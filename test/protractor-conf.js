f2c07872046b2abc85ded3a70500558574cb6b29
SOS1617_03_IRG <ivan_rondan@hotmail.com>
2017-04-25 12:29:22 +0000

    Tests

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,7 +1,7 @@
 exports.config = {   
     seleniumAddress: 'http://localhost:9515',
 
-    specs: ['T01-LoadResources.js','T02-AddResource.js'],
+    specs: ['T01-LoadResources.js','T02-AddResource.js','T01-LoadInvestmentsEducation.js','T02-AddInvestment.js'],
 
     capabilities: {
         'browserName': 'phantomjs'
