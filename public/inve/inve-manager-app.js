e694dfcc8455fbc6d8fc73770d1fbc6dbb287eab
SOS1617_03_IRG <ivan_rondan@hotmail.com>
2017-04-25 15:21:59 +0000

    Vistas creadas

--- public/inve/inve-manager-app.js
+++ public/inve/inve-manager-app.js
@@ -1,3 +1,17 @@
-angular.module("InvestmentsEducationApp",[]);
+angular.module("InvestmentsEducationApp",["ngRoute"]).config(function($routeProvider){
+    
+    $routeProvider
+    .when("/",{
+        templateUrl : "list.html",
+        controller : "ListCtrl"
+    })
+    .when("/investmentseducation/:country/:year",{
+        templateUrl : "edit.html",
+        controller : "EditCtrl"
+    });
+    
+    
+    console.log("App initialized and configured");
+    
+});
 
-console.log("App initialized");
