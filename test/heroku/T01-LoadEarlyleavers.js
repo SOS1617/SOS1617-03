96d3d139ea1dbaf9abd9654e34729afa0e1da1d0
SOS1617_rvs <rvizuetes@gmail.com>
2017-04-26 07:48:47 +0000

    Feedback OK

--- test/heroku/T01-LoadEarlyleavers.js
+++ test/heroku/T01-LoadEarlyleavers.js
@@ -1,6 +1,6 @@
 describe('Data is loaded', function () {
 	it('should show a bunch of data', function (){
-		browser.get('https://sos1617-03.herokuapp.com/esl/index.html#!/');
+		browser.get('https://sos1617-03.herokuapp.com/#!/esl/');
 		var earlyleavers = element.all(by.repeater('dataUnit in data'));
 
 		expect(earlyleavers.count()).toBeGreaterThan(2);
--- test/heroku/T02-AddEarlyleaver.js
+++ test/heroku/T02-AddEarlyleaver.js
@@ -1,6 +1,6 @@
 describe('Add result', function () {
 	it('should add a new result', function (){
-		browser.get('https://sos1617-03.herokuapp.com/esl/index.html#!/');
+		browser.get('https://sos1617-03.herokuapp.com/#!/esl/');
 
 		element.all(by.repeater('dataUnit in data')).then(function (initialEarlyleavers){
 				browser.driver.sleep(2000);
