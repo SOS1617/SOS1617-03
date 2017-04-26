ad1b91933973a84edb70ea8c65e32aedf5e4a467
SOS1617_03_IRG <ivan_rondan@hotmail.com>
2017-04-26 00:14:54 +0000

    Cambiando nombres a controladores

--- test/heroku/T01-LoadInvestments.js
+++ test/heroku/T01-LoadInvestments.js
@@ -1,6 +1,6 @@
 describe('Data is loaded', function () {
 	it('should show a bunch of data', function (){
-		browser.get('https://sos1617-03-irg-sanbox-sos161703irg.c9users.io/inve/index.html');
+		browser.get('https://sos1617-03.herokuapp.com/inve/index.html');
 		var results = element.all(by.repeater('dataUnit in data'));
 
 		expect(results.count()).toBeGreaterThan(3);
