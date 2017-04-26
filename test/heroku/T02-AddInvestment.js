3418a0eb92bc1c448a98d65e719d11c9e6c3f092
SOS1617_03_IRG <ivan_rondan@hotmail.com>
2017-04-26 00:12:03 +0000

    Cambiando nombres a controladores

--- test/heroku/T02-AddInvestment.js
+++ test/heroku/T02-AddInvestment.js
@@ -2,7 +2,7 @@ describe('Add data', function () {
 	it('should add a new data', function (){
 		browser.get('https://sos1617-03-irg-sanbox-sos161703irg.c9users.io/inve/index.html');
 
-		element.all(by.repeater('dataUnit in data')).then(function (initialResults){
+		element.all(by.repeater('dataUnit in data')).then(function (initialInvestmentsEducation){
 				browser.driver.sleep(2000);
 	
 				element(by.model('newData.country')).sendKeys('Letonia');
@@ -13,8 +13,8 @@ describe('Add data', function () {
 				
 				element(by.buttonText('add')).click().then(function (){
 
-					element.all(by.repeater('dataUnit in data')).then(function (results){
-						expect(results.length).toEqual(initialResults.length+1);
+					element.all(by.repeater('dataUnit in data')).then(function (investmentsEducation){
+						expect(investmentsEducation.length).toEqual(initialInvestmentsEducation.length+1);
 					});
 				
 				});
