4b75a430357a453afd657cc967f5381aa893db16
SOS1617_rvs <rvizuetes@gmail.com>
2017-04-26 00:13:56 +0000

    Funcionaaa

--- test/heroku/T02-AddEarlyleaver.js
+++ test/heroku/T02-AddEarlyleaver.js
@@ -5,12 +5,12 @@ describe('Add result', function () {
 		element.all(by.repeater('dataUnit in data')).then(function (initialEarlyleavers){
 				browser.driver.sleep(2000);
 	
-				element(by.model("newData.country")).sendKeys("Andorra");
-				element(by.model("newData.year")).sendKeys("2014");
-				element(by.model("newData['eslmale']")).sendKeys("20");
-				element(by.model("newData['eslfemale']")).sendKeys("30");
-				element(by.model("newData['esltotal']")).sendKeys("25");
-				element(by.model("newData['eslobjective']")).sendKeys("23");
+				element(by.model('newData.country')).sendKeys('Andorra');
+				element(by.model('newData.year')).sendKeys('2014');
+				element(by.model('newData.eslmale')).sendKeys('20');
+				element(by.model('newData.eslfemale')).sendKeys('30');
+				element(by.model('newData.esltotal')).sendKeys('25');
+				element(by.model('newData.eslobjective')).sendKeys('20');
 				
 				element(by.buttonText('add')).click().then(function (){
 
