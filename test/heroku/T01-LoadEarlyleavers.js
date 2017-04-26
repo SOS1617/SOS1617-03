ae1c00f45fad43e6dcc178f3e789edfdceb4abfe
SOS1617_rvs <rvizuetes@gmail.com>
2017-04-26 00:08:06 +0000

    A

--- test/heroku/T01-LoadEarlyleavers.js
+++ test/heroku/T01-LoadEarlyleavers.js
@@ -1,7 +1,8 @@
 describe('Data is loaded', function () {
 	it('should show a bunch of data', function (){
-		browser.get('https://sos1617-03.herokuapp.com/#!/esl');
-		var eslDataList = element.all(by.repeater('dataUnit in data'));
-		expect(eslDataList.count()).toBeGreaterThan(1);
+		browser.get('https://sos1617-03.herokuapp.com/esl/index.html#!/');
+		var earlyleavers = element.all(by.repeater('dataUnit in data'));
+
+		expect(earlyleavers.count()).toBeGreaterThan(2);
 	});
 });
