e1118a3e8a5c383cc7a6602709eb57b37f363e72
SOS1617_rvs <rvizuetes@gmail.com>
2017-05-02 18:38:21 +0000

    Cambios

--- public/esl/esl-graph-ctrl.js
+++ public/esl/esl-graph-ctrl.js
@@ -3,10 +3,39 @@ angular
     .controller("EslGraphCtrl",["$scope","$http",function ($scope, $http){
         
         $scope.apikey = "apisupersecreta";
+        $scope.data = {};
+        var dataCache = {};
+        $scope.categorias = [];
+        $scope.eslmale = [];
+        $scope.eslfemale = [];
+        $scope.esltotal = [];
+        $scope.eslobjective = [];
         
-        $http.get("/api/v2/earlyleavers/"+ "?" + "apikey=" + $scope.apikey).then(function(res){
-            console.log(res.data);
-            /*Highcharts.chart('container',{
+        function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+        
+        $http.get("/api/v2/earlyleavers/"+ "?" + "apikey=" + $scope.apikey).then(function(response){
+            
+            dataCache = response.data;
+            $scope.data = dataCache;
+            
+            for(var i=0; i<response.data.length; i++){
+                $scope.categorias.push(capitalizeFirstLetter($scope.data[i].country));
+                $scope.eslmale.push(Number($scope.data[i].eslmale));
+                $scope.eslfemale.push(Number($scope.data[i].eslfemale));
+                $scope.esltotal.push(Number($scope.data[i].esltotal));
+                $scope.eslobjective.push(Number($scope.data[i].eslobjective));
+                
+                console.log($scope.data[i].country);
+            }
+        });    
+            
+        console.log("Controller initialized");
+        $http.get("/api/v2/earlyleavers/"+ "?" + "apikey=" + $scope.apikey).then(function(response){
+            
+            
+            Highcharts.chart('container',{
                 title: {
                     text: 'Highcharts'
                 },
@@ -14,7 +43,7 @@ angular
                     type: 'bar'
                 },
                 xAxis: {
-                    categories: res.country
+                    categories: $scope.categorias
                 },
                 legend: {
                     layout: 'vertical',
@@ -28,22 +57,32 @@ angular
                 tooltip: {
                     formatter: function () {
                         return '<b>' + this.series.name + '</b><br/>' +
-                            this.x + ': ' + this.y;
+                            capitalizeFirstLetter(this.x) + ': ' + this.y;
                     }
                 },
                 series:[{
-                    data: res.esltotal.map(function(d){return Number(d.esltotal);})
+                    name: 'ESL Male',
+                    data: $scope.eslmale
+                }, {
+                    name: 'ESL Female',
+                    data: $scope.eslfemale
+                }, {
+                    name: 'ESL Total',
+                    data: $scope.esltotal
+                }, {
+                    name: 'ESL Objective',
+                    data: $scope.eslobjective
                 }]
-            });*/
+            });
             
             google.charts.load('current', {
                 'packages': ['geochart']
             });
             google.charts.setOnLoadCallback(drawRegionsMap);
                 function drawRegionsMap() {
-                    var myData = [['Country', 'ESLObjective']];
-                    res.data.forEach(function (d){
-                        myData.push([d.country,d.eslobjective]);
+                    var myData = [['Country', 'ESLTotal']];
+                    response.data.forEach(function (d){
+                        myData.push([d.country,Number(d.esltotal)]);
                     });
                     var data = google.visualization.arrayToDataTable(myData);
                     var options = {
