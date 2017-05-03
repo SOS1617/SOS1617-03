5d4c3eef63cf7d852e03c535d2f9b801bb8edcab
SOS1617_rvs <rvizuetes@gmail.com>
2017-05-03 01:11:34 +0000

    graficas casi hechas

--- public/esl/esl-graph-ctrl.js
+++ public/esl/esl-graph-ctrl.js
@@ -21,7 +21,7 @@ angular
             $scope.data = dataCache;
             
             for(var i=0; i<response.data.length; i++){
-                $scope.categorias.push(capitalizeFirstLetter($scope.data[i].country));
+                $scope.categorias.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
                 $scope.eslmale.push(Number($scope.data[i].eslmale));
                 $scope.eslfemale.push(Number($scope.data[i].eslfemale));
                 $scope.esltotal.push(Number($scope.data[i].esltotal));
@@ -79,11 +79,12 @@ angular
             google.charts.load('current', {
                 'packages': ['geochart']
             });
+            
             google.charts.setOnLoadCallback(drawRegionsMap);
                 function drawRegionsMap() {
                     var myData = [['Country', 'ESLTotal']];
                     response.data.forEach(function (d){
-                        myData.push([d.country,Number(d.esltotal)]);
+                        myData.push([capitalizeFirstLetter(d.country),Number(d.esltotal)]);
                     });
                     var data = google.visualization.arrayToDataTable(myData);
                     var options = {
@@ -92,5 +93,96 @@ angular
                     var chart = new google.visualization.GeoChart(document.getElementById('map'));
                     chart.draw(data, options);
                 }
+                
+            
+            var myChart = echarts.init(document.getElementById('echarts'));
+
+                // specify chart configuration item and data
+            var option2 = {
+                backgroundColor: '#0f375f',
+                title: {
+                    text: 'ECharts',
+                    textStyle:{
+                        color: '#ccc'
+                    }
+                },
+                tooltip: {},
+                legend: {
+                    data:['ESL Total','ESL Objective'],
+                    textStyle: {
+                        color: '#ccc'
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    data: $scope.categorias,
+                    axisLine: {
+                        lineStyle: {
+                            color: '#ccc'
+                        }
+                    }
+                },
+                yAxis: {
+                    splitLine: {show: false},
+                    axisLine: {
+                        lineStyle: {
+                            color: '#ccc'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'ESL Total',
+                    type: 'line',
+                    smooth: true,
+                    showAllSymbol: true,
+                    symbol: 'emptyCircle',
+                    symbolSize: 15,
+                    data: $scope.esltotal
+                }, {
+                    name: 'ESL Objective',
+                    type: 'bar',
+                    barWidth: 10,
+                    itemStyle: {
+                        normal: {
+                            barBorderRadius: 5,
+                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1,[{offset: 0, color: '#14c8d4'},
+                                {offset: 1, color: '#43eec6'}])
+                        }
+                    },
+                    data: $scope.eslobjective
+                }, {
+                    name: 'ESL Total',
+                    type: 'bar',
+                    barGap: '-100%',
+                    barWidth: 10,
+                    itemStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1,[
+                                {offset: 0, color: 'rgba(20,200,212,0.5)'},
+                                {offset: 0.2, color: 'rgba(20,200,212,0.2)'},
+                                {offset: 1, color: 'rgba(20,200,212,0)'}])
+                        }
+                    },
+                    z: -12,
+                    data: $scope.esltotal
+                }, {
+                    name: 'ESL Total',
+                    type: 'pictorialBar',
+                    symbol: 'rect',
+                    itemStyle: {
+                        normal: {
+                            color: '#0f375f'
+                        }
+                    },
+                    symbolRepeat: true,
+                    symbolSize: [12, 4],
+                        symbolMargin: 1,
+                    z: -10,
+                    data: $scope.esltotal
+                }]
+            };
+
+            // use configuration item and data specified to show chart
+            myChart.setOption(option2);
             });
     }]);
