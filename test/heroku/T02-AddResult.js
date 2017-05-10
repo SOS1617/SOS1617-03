ffd9fc49ed7232346815bcfc3d7bbfeabcd12b83
SOS1617_rrm <rubenramos93@hotmail.com>
2017-05-10 08:06:19 +0000

    fix

--- test/heroku/T02-AddResult.js
+++ test/heroku/T02-AddResult.js
@@ -1,6 +1,7 @@
 describe('Add result', function () {
 	it('should add a new result', function (){
 		browser.get('https://sos1617-03.herokuapp.com/#!/res');
+								element(by.linkText('4')).click();
 
 		element.all(by.repeater('dataUnit in data')).then(function (initialResults){
 				browser.driver.sleep(2000);
@@ -14,9 +15,10 @@ describe('Add result', function () {
 				
 				
 				element(by.buttonText('add')).click().then(function (){
+						element(by.linkText('4')).click();
 
 					element.all(by.repeater('dataUnit in data')).then(function (results){
-						expect(results.length).toEqual(initialResults.length);
+						expect(results.length).toEqual(initialResults.length+1);
 					});
 				
 				});
