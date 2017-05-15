71c09914323cc3568b429fd63429b5641929def4
SOS1617_rrm <rubenramos93@hotmail.com>
2017-05-15 14:51:04 +0000

    fix

--- public/res/res-external-chart-ctrl.js
+++ public/res/res-external-chart-ctrl.js
@@ -8,92 +8,155 @@ angular
         $scope.data = {};
         var dataCache = {};
          $scope.categorias = [];
+        $scope.gdp_per_capita_growth = [];
+        $scope.gdp_per_capita = [];
+        $scope.gdp_per_capita_ppp = [];
          $scope.reading = [];
          $scope.science = [];
          $scope.math = [];
          
-         $http.get("../api/v2/results?apikey=" + $scope.apikey).then(function(response){
+         $http.get("https://sos1617-06.herokuapp.com/api/v1/gdp-per-capita?apikey=secret").then(function(response){
              dataCache = response.data;
              $scope.data = dataCache;
              
              for(var i=0; i<response.data.length; i++){
+                 if($scope.data[i].country != "morocco"){
                  $scope.categorias.push($scope.data[i].country + " " +  $scope.data[i].year);
-                 $scope.reading.push(Number($scope.data[i].reading));
-                 $scope.science.push(Number($scope.data[i].science));
-                 $scope.math.push(Number($scope.data[i].math));
+                 $scope.gdp_per_capita.push(Number($scope.data[i]["gdp-per-capita"]));
+                 $scope.gdp_per_capita_ppp.push(Number($scope.data[i]["gdp-per-capita-ppp"]));
+                 $scope.gdp_per_capita_growth.push(Number($scope.data[i]["gdp-per-capita-growth"]));
 
-
+}
                  console.log($scope.data[i].country);
              }
              
 
 
          })
+         
+                  $http.get("../api/v2/results?apikey=" + $scope.apikey).then(function(response){
+             dataCache = response.data;
+             $scope.data = dataCache;
+             
+             for(var i=0; i<response.data.length; i++){
+                 if(($scope.data[i].country == "Poland" && $scope.data[i].year == "2015" )|| ($scope.data[i].country == "Spain" && $scope.data[i].year == "2015") ||
+                 ($scope.data[i].country == "France" && $scope.data[i].year == "2015" )){
+                 $scope.reading.push(Number($scope.data[i].reading));
+                 $scope.science.push(Number($scope.data[i].science));
+                 $scope.math.push(Number($scope.data[i].math));
+}
+
+                  console.log($scope.data[i].country);
+
+             }
+             
+
+
+         })
+         
+         
 
                 console.log("Controller intialized");
                 $http
-                    .get("../api/v2/results?apikey=" + $scope.apikey)
+                    .get("https://sos1617-06.herokuapp.com/api/v1/gdp-per-capita?apikey=secret")
                     .then(function(response) {
                         console.log("hola" + response.data);
-                    Highcharts.chart('reschartexternal', {
-                                chart: {
-                                    type: 'column'
-                                },
-                                title: {
-                                    text: 'PISA results'
-                                },
-                                xAxis: {
-                                  
-                                    categories: $scope.categorias
-                                
-                                },
-                                yAxis: {
-                                    min: 0,
-                                    title: {
-                                        text: 'Comparison between countries'
-                                    },
-                                    stackLabels: {
-                                        enabled: true,
-                                        style: {
-                                            fontWeight: 'bold',
-                                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                                        }
-                                    }
-                                },
-                                legend: {
-                                    align: 'right',
-                                    x: -30,
-                                    verticalAlign: 'top',
-                                    y: 25,
-                                    floating: true,
-                                    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-                                    borderColor: '#CCC',
-                                    borderWidth: 1,
-                                    shadow: false
-                                },
-                                tooltip: {
-                                    headerFormat: '<b>{point.x}</b><br/>',
-                                    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-                                },
-                                plotOptions: {
-                                    column: {
-                                        stacking: 'normal',
-                                        dataLabels: {
-                                            enabled: true,
-                                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-                                        }
-                                    }
-                                },
-                                series: [{
-                                    name: 'Reading',
-                                    data: $scope.reading
-                                }, {
-                                    name: 'Science',
-                                    data: $scope.science
-                                }, {
-                                    name: 'Math',
-                                    data: $scope.math
-                                }]
-                            });
+
+
+Highcharts.chart('reschartexternal', {
+    chart: {
+        zoomType: 'xy'
+    },
+    title: {
+        text: 'Comparison between Pisa Results and GDP per capita'
+    },
+    xAxis: [{
+        categories: $scope.categorias,
+        crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+        labels: {
+          //  format: '{value}°C',
+            style: {
+                color: Highcharts.getOptions().colors[2]
+            }
+        },
+        title: {
+            text: 'GDP per capita',
+            style: {
+                color: Highcharts.getOptions().colors[2]
+            }
+        },
+        opposite: true
+
+    }, { // Secondary yAxis
+        gridLineWidth: 0,
+        title: {
+            text: 'PISA results',
+            style: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        },
+        labels: {
+         //   format: '{value} mm',
+            style: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        }
+
+    }, { // Tertiary yAxis
+        gridLineWidth: 0,
+        title: {
+            text: 'GDP per capita, PPP',
+            style: {
+                color: Highcharts.getOptions().colors[1]
+            }
+        },
+        labels: {
+          //  format: '{value} mb',
+            style: {
+                color: Highcharts.getOptions().colors[1]
+            }
+        },
+        opposite: true
+    }],
+    tooltip: {
+        shared: true
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'center',
+        x: 80,
+        verticalAlign: 'top',
+        y: 55,
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    series: [{
+        name: 'PISA results',
+        type: 'spline',
+        yAxis: 1,
+        data: $scope.reading,
+
+
+    }, {
+        name: 'GDP per capita',
+        type: 'spline',
+        yAxis: 2,
+        data: $scope.gdp_per_capita,
+        marker: {
+            enabled: false
+        },
+        dashStyle: 'shortdot',
+
+
+    }, {
+        name: 'GDP per capita, PPP',
+        type: 'spline',
+        data: $scope.gdp_per_capita_ppp,
+
+    }]
+});
                     });
             }]);
+
