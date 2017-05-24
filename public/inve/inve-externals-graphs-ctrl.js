b305bba44fc545f10857973aba868fa2d0e0719e
SOS1617_03_IRG <ivan_rondan@hotmail.com>
2017-05-24 08:02:45 +0000

    Integracion apis  externas

--- public/inve/inve-externals-graphs-ctrl.js
+++ public/inve/inve-externals-graphs-ctrl.js
@@ -9,6 +9,7 @@ angular
         $scope.latitude = [];  ///// []
         $scope.dataAPI1 = [];  ///// []
         $scope.dataAPI2 = [];
+        $scope.res = [];
         $scope.number = [];
         $scope.dataIE = {};
         var dataCacheAPI1 = {};
@@ -17,6 +18,7 @@ angular
         var res = {};
         $scope.categorias = [];
         $scope.categorias1 = [];
+        $scope.countries = [];
         $scope.population = [];
         $scope.riskpoverty = [];
         $scope.inveducation = [];
@@ -59,6 +61,7 @@ angular
                 
                 for(var i=0; i<5; i++){
                      $scope.categorias1.push($scope.data[i].country + " " +  $scope.data[0].year);
+                     $scope.countries.push($scope.data[i].country);
                      $scope.population.push(Number($scope.data[i].population));
                      $scope.riskpoverty.push(Number($scope.data[i].riskpoverty));
                      $scope.inveducation.push(Number($scope.data[i].inveducation));
@@ -74,7 +77,7 @@ angular
 
 
                     ////HIGHCHARTs////
-
+                    /*
                     Highcharts.chart('container',{
                         title: {
                             text: 'INVESTMENTS IN EDUCATION integrated with external API1'
@@ -117,8 +120,9 @@ angular
                             name: 'Latitude',
                             data: $scope.latitude,
                         }]
-                    });
+                    });*/
                 
+                /*
                 Highcharts.chart('container2', {
                                 chart: {
                                     plotBackgroundColor: null,
@@ -159,12 +163,13 @@ angular
                                     }]
                                 }]
                             });
-                            
+                            */
                             
                             //////dygraphs //////////////
 
-                          var fila1 = [1,Number($scope.inveducation[0]),Number($scope.latitude[0])];
-                          var fila2 = [2,Number($scope.inveducation[1]),Number($scope.latitude[1])];
+                          var fila1 = [2012,Number($scope.inveducation[0]),Number($scope.latitude[0])];
+                          var fila2 = [2013,Number($scope.inveducation[1]),Number($scope.latitude[1])];
+                          var fila3 = [2014,Number($scope.inveducation[2]),Number($scope.latitude[2])];
                              var g = new Dygraph(
 
                                 // containing div
@@ -172,41 +177,35 @@ angular
                                 [
                             fila1,
                             fila2,
+                            fila3
                               ],
                               {
                                 labels: [ "country", "inveducation" , "latitude"]
                               });    
                               
                               
-                /////////////TAUCHART //////////////////////////////              
-                              
-                $scope.sta = [];
+                /////////////CHART JS //////////////////////////////              
                 
-                            $http
-                .get("https://numbersapi.p.mashape.com/6/21/date?fragment=true&json=true&mashape-key=oz6U9F2JarmshGsOl9ZxXvOG8CEsp1DFgAmjsnmEE7ChuL3GIB")
-                .then(function(response) {
-                    $scope.sta.push(Number(response.data.number));
-                         var chart = new tauCharts.Chart({
-                            type: 'scatterplot',
-                            x: 'importS',
-                            y: 'exportS',
-                            color: 'provinces',
-                            data: $scope.sta,
-                            plugins: [
-                                tauCharts.api.plugins.get('tooltip')({
-                                    fields: ["country", "year"]
-                                }),
-                                tauCharts.api.plugins.get('legend')()
-                            ]
-                        });
-                        chart.renderTo('#scatter');
-                    },
-                    function(response) {
-                        $scope.sta = [];
-                    });              
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                  type: 'line',
+                  data: {
+                    labels: ['Spain 2014', 'France 2014'],
+                    datasets: [{
+                      label: 'numbers',
+                      data: [Number($scope.number), Number($scope.number)],
+                      backgroundColor: "rgba(153,255,51,0.6)"
+                    }, {
+                      label: 'inveducation',
+                      data: [Number($scope.inveducation[0]),Number($scope.inveducation[1])],
+                      backgroundColor: "rgba(255,153,0,0.6)"
+                    }]
+                  }
+                });               
+            
+                //////////////////////////////////////////////////
                 
-                });
-                
+                });   
             });
          
      });
